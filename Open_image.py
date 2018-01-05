db81617a6a21b1d8bbca66c134934c3a94743b8a
David Hinton <davidhinton@Davids-MacBook-Pro.local>
2018-01-05 14:29:39 -0500

    updated code using Pylint

--- Open_image.py
+++ Open_image.py
@@ -1,23 +1,23 @@
-#Automating Instagram with Python and Selenium - EuroPython2017
-from time import sleep
+"""This is a docstring - required by PEP 257 -
+    Source: Automating Instagram with Python and Selenium - EuroPython2017
+
+    This script is used to parse IDX - webserver appication for matching
+    oil and gas leases, then provide them to the google Vision API to
+    update current records.
+"""
+from Vision_URL import myVision
 from selenium import webdriver
-
-#handling new window - see method: new_window()
-from selenium.webdriver.support import expected_conditions as EC
-
+# error on action chains with out import
+from selenium.webdriver.common.action_chains import ActionChains
+# handling new window - see method: new_window()
+#from selenium.webdriver.support import expected_conditions as EC
 # waits - else it fails to load data
 from selenium.webdriver.support.ui import WebDriverWait
-
-#error on action chains with out import
-from selenium.webdriver.common.action_chains import ActionChains
-
-from Vision_URL import myVision
-
-
+from time import sleep
 
 
 class MarshallCounty():
-
+    """This is a docstring - required by PEP 257"""
     def __init__(self, web_address):
         # starting a new browser session
         self.browser = webdriver.Chrome()
@@ -25,15 +25,16 @@ class MarshallCounty():
         self.image_urls = []
 
     def open_to_site(self):
+        """Using selenium to open site"""
         self.browser.get(self.address)
 
-        
-    def input_last(self,last_name):
+    def input_last(self, last_name):
+        """Search IDX base on last name"""
         # find the input and data entered
         # note: had to enable Right-Clicking on Individual Sites with a Bit of Your Own JavaScript
         # https://www.howtogeek.com/248731/how-to-enable-right-clicking-on-web-sites-that-block-it/
         # Working: javascript:void(document.oncontextmenu=null);
-        lname_input = self.browser.find_element_by_xpath\
+        lname_input = self.browser.find_element_by_xpath \
             ('//*[@id="CallFormPanel_contentSplitter_CallToolPanel_txtLname_I"]')
 
         ActionChains(self.browser) \
@@ -42,6 +43,7 @@ class MarshallCounty():
             .perform()
 
     def preform_search(self):
+        """running the query and waiting for results to process"""
         # find the search index button and click it
         search_index = self.browser.find_element_by_xpath(
             '//*[@id="CallFormPanel_contentSplitter_CallToolPanel_rc_T0G2I2_LI"]')
@@ -53,71 +55,76 @@ class MarshallCounty():
         # Wait for AJAX to load before parsing data
         self.browser.implicitly_wait(5)
 
-
     def parse_data(self):
-        counter = 1
+        """Analysing the data"""
+        ##counter = 1  --- for use with spreadsheets
         for i in range(0, 100):
             sub = self.browser.find_element_by_xpath(
-                '//*[@id="CallFormPanel_contentSplitter_grd_DXDataRow' + str(i) + '"]/td[5]') #Description
-            #print(sub.get_attribute("innerHTML"))
+                # Description
+                '//*[@id="CallFormPanel_contentSplitter_grd_DXDataRow' + str(i) + '"]/td[5]')
+            # print(sub.get_attribute("innerHTML"))
             sub.click()
-            if (sub.get_attribute("innerHTML") == "OIL &amp; GAS LEASE"):
+            if sub.get_attribute("innerHTML") == "OIL &amp; GAS LEASE":
                 self.new_window(i)
-                
-                name = self.browser.find_element_by_xpath(
-                    '//*[@id="CallFormPanel_contentSplitter_grd_DXDataRow' + str(i) + '"]/td[9]')
 
-                description = self.browser.find_element_by_xpath('//*[@id="CallFormPanel_contentSplitter_grd_DXDataRow'
-                                                            + str(i) + '"]/td[10]')
+                name = self.browser.find_element_by_xpath\
+                ('//*[@id="CallFormPanel_contentSplitter_grd_DXDataRow'
+                 + str(i) + '"]/td[9]')
 
-                instrument = self.browser.find_element_by_xpath('//*[@id="CallFormPanel_contentSplitter_grd_DXDataRow'
-                                                           + str(i) + '"]/td[11]')
+                description = self.browser.find_element_by_xpath\
+                    ('//*[@id="CallFormPanel_contentSplitter_grd_DXDataRow'
+                     + str(i) + '"]/td[10]')
 
+                instrument = self.browser.find_element_by_xpath\
+                    ('//*[@id="CallFormPanel_contentSplitter_grd_DXDataRow'
+                     + str(i) + '"]/td[11]')
 
         # make sure the browser stays open for 20 seconds
         sleep(20)
         # clean exit
         self.browser.close()
 
-    def new_window(self,counter):
+    def new_window(self, counter):
+        """Handling the new windows and retriving url for Vision API"""
         window_before_click = self.browser.window_handles[0]
-        #clicking a tag
-        self.browser.find_element_by_xpath('//*[@id="CallFormPanel_contentSplitter_grd_DXDataRow' + str(counter) + '"]/td/a').click()        #WebDriverWait(self.browser,10).until(EC.number_of_windows_to_be(1))
+
+        # clicking a tag
+        self.browser.find_element_by_xpath\
+            ('//*[@id="CallFormPanel_contentSplitter_grd_DXDataRow' + str(
+                counter) + '"]/td/a').click()
+        # WebDriverWait(self.browser,10).until(EC.number_of_windows_to_be(1))
         new_window = self.browser.window_handles[1]
-        #switching to new window
+        # switching to new window
         self.browser.switch_to_window(new_window)
         self.javascript_url()
-        #close new window
+        # close new window
         sleep(4)
         self.browser.close()
-        #switch back to main window
+        # switch back to main window
         self.browser.switch_to_window(window_before_click)
-       
 
     def javascript_url(self):
-        #Retrieving url via javascript -- return is required to set variable
+        """method to get URL to image"""
+        # Retrieving url via javascript -- return is required to set variable
         url = self.browser.execute_script("return document.images[50].getAttribute('src')")
-        self.image_urls.append("URL: http://129.71.117.225{}".format(url)) #--- look like the link expire
+        # --- look like the link expires
+        self.image_urls.append("URL: http://129.71.117.225{}".format(url))
+
 
 ##       ACTIVATE VISION API
-        
 ##        new = myVision()
 ##        new.detect_document_uri("http://129.71.117.225{}".format(url))
-        
+
 
 def main():
-    m1 = MarshallCounty('http://129.71.117.225/')
-    m1.open_to_site()
-    m1.input_last('smith')
-    m1.preform_search()
-    m1.parse_data()
-    for item in m1.image_urls:
+    """Running"""
+    run1 = MarshallCounty('http://129.71.117.225/')
+    run1.open_to_site()
+    run1.input_last('smith')
+    run1.preform_search()
+    run1.parse_data()
+    for item in run1.image_urls:
         print(item)
 
 if __name__ == '__main__':
     main()
-
-
-#TODO: - image OCR to search for lease numbers
-#https://stackoverflow.com/questions/13437727/python-write-to-excel-spreadsheet
-#https://www.youtube.com/watch?v=FbWnuO7GGBQ
